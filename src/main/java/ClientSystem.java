91278c06abc96cb2303508b03247528f740a2384
myushit777 <myushit777@gmail.copm>
2024-05-15 11:20:03 +0200

    huhu

--- src/main/java/ClientSystem.java
+++ src/main/java/ClientSystem.java
@@ -5,13 +5,11 @@ import java.util.Random;
 public class ClientSystem {
     private static final int MIN_DELAY = 3000; // Minimale Verzögerung in Millisekunden (3 Sekunden)
     private static final int MAX_DELAY = 6000; // Maximale Verzögerung in Millisekunden (6 Sekunden)
-    private static final int NUM_THREADS = 10; // Anzahl der Threads, die gleichzeitig laufen
 
     public static void main(String[] args) {
         // Erstellen und Hinzufügen von HotelBookingServices und Hotels
         HotelBookingService service1 = new HotelBookingService();
         HotelBookingService service2 = new HotelBookingService();
-        HotelBookingService service3 = new HotelBookingService();
 
         service1.addHotel(new Hotel("H001", "Hotel California", 50));
         service1.addHotel(new Hotel("H002", "Hotel Nevada", 30));
@@ -25,53 +23,29 @@ public class ClientSystem {
         service2.addHotel(new Hotel("H009", "Hotel Michigan", 30));
         service2.addHotel(new Hotel("H010", "Hotel Ohio", 20));
 
-        service3.addHotel(new Hotel("H011", "Hotel Seattle", 45));
-        service3.addHotel(new Hotel("H012", "Hotel Denver", 50));
-        service3.addHotel(new Hotel("H013", "Hotel Boston", 30));
-        service3.addHotel(new Hotel("H014", "Hotel Philadelphia", 25));
-        service3.addHotel(new Hotel("H015", "Hotel Las Vegas", 40));
-
         // Starten einer Endlosschleife zur kontinuierlichen Generierung von Buchungsanfragen
-        for (int i = 0; i < NUM_THREADS; i++) {
-            new Thread(() -> {
-                int tripCounter = 1;
-                Random random = new Random();
-                while (true) {
-                    List<Thread> threads = new ArrayList<>();
-                    int finalTripCounter = tripCounter;
-                    threads.add(new Thread(() -> sendTripRequests("Reise " + finalTripCounter, createTrip(service1, new String[]{"H001", "H002", "H003", "H004", "H005"}))));
-                    threads.add(new Thread(() -> sendTripRequests("Reise " + finalTripCounter, createTrip(service2, new String[]{"H006", "H007", "H008", "H009", "H010"}))));
-                    threads.add(new Thread(() -> sendTripRequests("Reise " + finalTripCounter, createTrip(service3, new String[]{"H011", "H012", "H013", "H014", "H015"}))));
-                    tripCounter++;
+        while (true) {
+            List<Thread> threads = new ArrayList<>();
+            threads.add(new Thread(() -> sendTripRequests("Reise 1", createTrip(service1, new String[]{"H001", "H002", "H003", "H004", "H005"}))));
+            threads.add(new Thread(() -> sendTripRequests("Reise 2", createTrip(service2, new String[]{"H006", "H007", "H008", "H009", "H010"}))));
 
-                    // Starten der Threads mit zufälligen Verzögerungen
-                    for (Thread thread : threads) {
-                        thread.start();
-                        try {
-                            int delay = MIN_DELAY + random.nextInt(MAX_DELAY - MIN_DELAY); // Zufällige Verzögerung zwischen den Starts der Reisen
-                            Thread.sleep(delay);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    // Warten auf Abschluss der Threads
-                    for (Thread thread : threads) {
-                        try {
-                            thread.join();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    // Verzögerung zwischen den Startzyklen der Reisen
-                    try {
-                        Thread.sleep(10000); // 10 Sekunden Verzögerung vor dem nächsten Zyklus
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+            // Starten der Threads mit zufälligen Verzögerungen
+            for (Thread thread : threads) {
+                thread.start();
+                try {
+                    int delay = MIN_DELAY + new Random().nextInt(MAX_DELAY - MIN_DELAY); // Zufällige Verzögerung zwischen den Starts der Reisen
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            }).start();
+            }
+
+            // Verzögerung zwischen den Startzyklen der Reisen
+            try {
+                Thread.sleep(10000); // 10 Sekunden Verzögerung vor dem nächsten Zyklus
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
